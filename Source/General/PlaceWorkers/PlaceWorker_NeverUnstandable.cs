0505fb523ab691915acac88fe1e2b45bc98afd31
JonnyNova <JonnyNova@users.noreply.github.com>
2019-11-06 10:44:13 -0500

    Add placeworker for passable cells only

---
+++ Source/General/PlaceWorkers/PlaceWorker_NeverUnstandable.cs
@@ -0,0 +1,16 @@
+using System.Linq;
+using Verse;
+
+namespace FrontierDevelopments.General.PlaceWorkers
+{
+    public class PlaceWorker_NeverUnstandable : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef def, IntVec3 center, Rot4 rot, Map map, Thing thingToIgnore = null)
+        {
+            return !GenAdj.OccupiedRect(center, rot, def.Size).Cells.Any(cell => 
+                map.thingGrid.ThingsAt(cell)
+                    .Where(thing => thing != thingToIgnore)
+                    .Any(thing => thing.def.passability == Traversability.Impassable));
+        }
+    }
+}
