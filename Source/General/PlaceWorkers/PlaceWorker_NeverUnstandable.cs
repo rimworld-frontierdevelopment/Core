fdadfb5c04129fac33d1af50ed3e1a13b3bf4d4c
JonnyNova <JonnyNova@users.noreply.github.com>
2020-09-13 13:12:22 -0400

    Fix compilation errors

--- Source/General/PlaceWorkers/PlaceWorker_NeverUnstandable.cs
+++ Source/General/PlaceWorkers/PlaceWorker_NeverUnstandable.cs
@@ -5,7 +5,7 @@ namespace FrontierDevelopments.General.PlaceWorkers
 {
     public class PlaceWorker_NeverUnstandable : PlaceWorker
     {
-        public override AcceptanceReport AllowsPlacing(BuildableDef def, IntVec3 center, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing)
+        public override AcceptanceReport AllowsPlacing(BuildableDef def, IntVec3 center, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
         {
             return !GenAdj.OccupiedRect(center, rot, def.Size).Cells.Any(cell => 
                 map.thingGrid.ThingsAt(cell)
