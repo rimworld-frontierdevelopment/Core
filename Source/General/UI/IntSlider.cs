e10e59584a597499a59d1c4878e311a04457cd1d
JonnyNova <JonnyNova@users.noreply.github.com>
2020-09-30 08:07:49 -0400

    Add mouse tracking to IntSlider

--- Source/General/UI/IntSlider.cs
+++ Source/General/UI/IntSlider.cs
@@ -11,6 +11,7 @@ namespace FrontierDevelopments.General.UI
         private readonly int _ceiling;
         private readonly Func<int> _current;
         private readonly Action<int> _onSet;
+        private readonly Action<bool> _hasMouse;
 
         public IntSlider(string label, int floor, int ceiling, Func<int> current, Action<int> onSet)
         {
@@ -19,10 +20,22 @@ namespace FrontierDevelopments.General.UI
             _ceiling = ceiling;
             _current = current;
             _onSet = onSet;
+            _hasMouse = null;
+        }
+
+        public IntSlider(string label, int floor, int ceiling, Func<int> current, Action<int> onSet, Action<bool> hasMouse)
+        {
+            _label = label;
+            _floor = floor;
+            _ceiling = ceiling;
+            _current = current;
+            _onSet = onSet;
+            _hasMouse = hasMouse;
         }
 
         public void Draw(Rect rect)
         {
+            _hasMouse?.Invoke(rect.Contains(Event.current.mousePosition));
             _onSet((int)Widgets.HorizontalSlider(
                 rect, 
                 _current(), 
